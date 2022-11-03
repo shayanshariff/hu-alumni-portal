899bb54cee1bbbc0d7701f6ed588ba03af5af338
shayanshariff <60033950+shayanshariff@users.noreply.github.com>
2022-11-04 03:43:25 +0500

    routes cleanup

--- routes/alumni.js
+++ routes/alumni.js
@@ -1,45 +1,21 @@
 import express from 'express';
-import { v4 as uuidv4 } from 'uuid';
+import { createAlumni } from '../controllers/alumni.js';
+import { getAlumni } from '../controllers/alumni.js';
+import { getAlumnis } from '../controllers/alumni.js';
+import { deleteAlumni } from '../controllers/alumni.js';
+import { updateAlumni } from '../controllers/alumni.js';
 
 const router = express.Router();
 
-let alumni = [
-    {
-    firstName: "John",
-    lastName: "Doe",
-    batch: 2021
-    },
-    {
-        firstName: "Jane",
-        lastName: "Doe",
-        batch: 2022
-        }
-]
 //Alumni routes already start with /alumni
-router.get('/', (req, res) => {
-    console.log(alumni);
-    res.send(alumni);
-});
+router.get('/:id', getAlumni);
 
-router.post('/', (req, res) => {
-    const alumniPost = req.body;
-    alumni.push({ ...alumniPost, id: uuidv4()});
-    res.send(`Alumni with the name ${alumniPost.firstName} added to the database`);
-})
+router.get('/', getAlumnis);
 
-router.get('/:id', (req, res) => {
-    const {id} = req.params;
+router.post('/', createAlumni);
 
-    
-    res.send(alumni.find((alumni) => alumni.id === id));
-})
+router.delete('/:id', deleteAlumni);
 
-router.delete('/:id', (req, res) => {
-    const {id} = req.params;
+router.patch('/:id', updateAlumni);
 
-    alumni = alumni.filter((alumni) => alumni.id != id)
-
-    res.send(`User with the id ${id} deleted from the database`);
-
-})
 export default router;
