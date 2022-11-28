6f4f2669ff543b73925972be7cda5358cd7df88a
shayanshariff <60033950+shayanshariff@users.noreply.github.com>
2022-11-29 03:34:21 +0500

    navbar v1

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -15,7 +15,38 @@ import ProfileLanding from "./profile-landing";
 
 function App() {
   return (
+
       <BrowserRouter>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark p-2" >
+  <a class="navbar-brand" href="#">
+    <img src="habib.png" width="30" height="30" class="d-inline-block align-top" alt=""/>
+    HU Alumni Portal
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
         <Routes>
           <Route exact path="/" element={<Landing />}/>
           <Route path="/login" element={<Login />}/>
