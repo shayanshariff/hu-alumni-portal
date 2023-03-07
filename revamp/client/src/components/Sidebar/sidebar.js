7122ecad89e78dc498205efc555d18d4680a11bc
aliadnan2000 <60284195+aliadnan2000@users.noreply.github.com>
2023-03-07 14:51:43 +0500

    sidebar

---
+++ revamp/client/src/components/Sidebar/sidebar.js
@@ -0,0 +1,23 @@
+// import React from "react";
+// import { ProSidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
+// import "react-pro-sidebar/dist/css/styles.css";
+
+// const Sidebar = ({ isSidebar }) => {
+//   return (
+//     <>
+//       {isSidebar && (
+//         <ProSidebar>
+//           <Menu iconShape="square">
+//             <MenuItem>Dashboard</MenuItem>
+//             <SubMenu title="Components">
+//               <MenuItem>Component 1</MenuItem>
+//               <MenuItem>Component 2</MenuItem>
+//             </SubMenu>
+//           </Menu>
+//         </ProSidebar>
+//       )}
+//     </>
+//   );
+// };
+
+// export default Sidebar;
