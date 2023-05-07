7e53c3a41ccf497dec9fc8c842892c4af90a23fb
Shayan Shariff <60033950+shayanshariff@users.noreply.github.com>
2023-05-07 18:54:29 +0500

    Edit profile functionality

--- revamp/client/src/App.js
+++ revamp/client/src/App.js
@@ -11,6 +11,7 @@ import Profile from "./components/Profile/Profile";
 import AlumniForum from "./components/AlumniForum/AlumniForum";
 import Dashboard from "./components/Dashboard";
 import Chat from "./components/Chat/Chat";
+import EditProfile from "./components/EditProfile/EditProfile";
 import { createTheme } from "@mui/material/styles";
 import { themeSettings } from "./theme";
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
@@ -19,7 +20,6 @@ import barchart from "./components/Graphs/indexbarchart";
 // import barChart_students_working from "./components/Graphs/indexbarchart(students working)";
 import Piechart from "./components/Charts/piechart";
 import Bar from "./components/Charts/bar";
-import EditProfile from './components/EditProfile/EditProfile';
 import StudentList from "./components/Charts/custom";
 const App = () => {
     const user = JSON.parse(localStorage.getItem('profile'));
@@ -28,23 +28,27 @@ const App = () => {
     return (
         <BrowserRouter>
             <ThemeProvider theme={theme}>
-                <Container maxwidth="false" disableGutters="true">
-                    <Navbar/>
-                    <Switch>
-                        <Route path="/posts" exact component={Home}/>
-                        <Route path="/posts/alumni" exact component={AlumniForum}/>
-                        <Route path="/breakdown" exact component={BreakDown}/>
-                        <Route path="/barchart" exact component={barchart}/>
-                        <Route path="/profile" exact component={Profile}/>
-                        <Route path="/dashboard" exact component={Dashboard}/>
-                        <Route path="/piechart" exact component={Piechart}/>
-                        <Route path="/bar" exact component={Bar}/>
-                        <Route path="/custom" exact component={StudentList}/>
-                        {/* <Route path="/barchart(studentsworking)" exact component={barChart_students_working}/> */}
-                        <Route path="/Chat" exact component={Chat}/>
-                        <Route path="/edit-profile" exact component={EditProfile} />
-                        <Route path="/" exact component={() => (!user ? <Auth /> : <Redirect to="/posts" />)} />
-                    </Switch>
+                <Container maxWidth="false" disableGutters="true">
+                <Box display="flex">
+  <Navbar />
+  <Box flexGrow={1}>
+    <Switch>
+    <Route path="/posts" exact component={Home}/>
+                            <Route path="/posts/alumni" exact component={AlumniForum}/>
+                            <Route path="/breakdown" exact component={BreakDown}/>
+                            <Route path="/barchart" exact component={barchart}/>
+                            <Route path="/profile" exact component={Profile}/>
+                            <Route path="/dashboard" exact component={Dashboard}/>
+                            <Route path="/piechart" exact component={Piechart}/>
+                            <Route path="/bar" exact component={Bar}/>
+                            <Route path="/custom" exact component={StudentList}/>
+                            <Route path="/edit-profile" exact component={EditProfile}/>
+                            {/* <Route path="/barchart(studentsworking)" exact component={barChart_students_working}/> */}
+                            <Route path="/Chat" exact component={Chat}/>
+                            <Route path="/" exact component={() => (!user ? <Auth /> : <Redirect to="/posts" />)} />
+    </Switch>
+  </Box>
+</Box>           
                 </Container>
             </ThemeProvider>
         </BrowserRouter>
