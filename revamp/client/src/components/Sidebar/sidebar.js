69a9a5cb5bac8640529966d7ef022ba8de6c6b04
aliadnan2000 <60284195+aliadnan2000@users.noreply.github.com>
2023-05-08 12:59:16 +0500

    backend deployment

--- revamp/client/src/components/Sidebar/sidebar.js
+++ revamp/client/src/components/Sidebar/sidebar.js
@@ -21,3 +21,124 @@
 // };
 
 // export default Sidebar;
+
+
+// import React, {useState, useEffect} from "react";
+// import {Avatar, Button, Divider, List, ListItem, ListItemIcon, ListItemText, Typography} from "@material-ui/core";
+// import {Link, useHistory, useLocation} from "react-router-dom";
+// import * as actionType from '../../constants/actionTypes';
+// import { useDispatch } from "react-redux";
+// import decode from 'jwt-decode';
+// import useStyles from "./styles";
+// import logo from "../../images/habib_logo.png";
+
+// import DashboardIcon from '@material-ui/icons/Dashboard';
+// import PeopleIcon from '@material-ui/icons/People';
+// import ChatIcon from '@material-ui/icons/Chat';
+// import ForumIcon from '@material-ui/icons/Forum';
+
+// const Navbar = () => {
+//   const classes = useStyles();
+//   const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+//   const dispatch = useDispatch();
+//   const history = useHistory();
+//   const [isVisible, setIsVisible] = useState(true);
+//   const location = useLocation();
+
+//   const toggleVisibility = () => {
+//     setIsVisible(prevState => !prevState);
+//   };
+
+//   const logout = () => {
+//     dispatch({type: actionType.LOGOUT});
+//     setUser(null);
+//     history.push("/");
+//   };
+
+//   useEffect(() => {
+//     const token = user?.token;
+//     if (token) {
+//       const decodedToken = decode(token);
+//       if (decodedToken.exp * 1000 < new Date().getTime()) logout();
+//     }
+//     setUser(JSON.parse(localStorage.getItem('profile')));
+//   // eslint-disable-next-line react-hooks/exhaustive-deps
+//   }, [location]);
+
+//   return (
+//     <div className={`${classes.sidebar} ${isVisible ? 'w-full md:w-64' : 'md:w-0'}`}>
+//       <div className={classes.brandContainer}>
+//         <img className={classes.image} src={logo} alt="habib-logo" height="60" />
+//         <Typography className={classes.heading} variant="h5" color="black">Habib University</Typography>
+//         <button
+//           className="fixed z-10 right-0 top-0 p-4 text-white bg-purple-900 hover:bg-purple-800"
+//           onClick={toggleVisibility}
+//         >
+//           {isVisible ? "Hide" : "Show"}
+//         </button>
+//       </div>
+//       <Divider />
+//       <List>
+//       <ListItem
+//           component={Link}
+//           to={{ pathname: '/profile', state: {user: user?.result } }}
+//           className={`${classes.listItem} ${location.pathname === '/profile' ? classes.active : ''}`}
+//           button
+//         >
+//           <ListItemIcon><Avatar className={classes.purple} alt={user?.result?.name} src={user?.result?.imageUrl}>{user?.result?.name.charAt(0)}</Avatar></ListItemIcon>
+//           <ListItemText primary={user?.result?.name} />
+//         </ListItem>
+//         <ListItem
+//           component={Link}
+//           to="/dashboard"
+//           className={`${classes.listItem} ${location.pathname === '/dashboard' ? classes.active : ''}`}
+//           button
+//         >
+//           <ListItemIcon><DashboardIcon /></ListItemIcon>
+//           <ListItemText primary="Dashboard" />
+//         </ListItem>
+        
+//         <ListItem
+//           component={Link}
+//           to="/Chat"
+//           className={`${classes.listItem} ${location.pathname === '/Chat' ? classes.active : ''}`}
+//           button
+//         >
+//           <ListItemIcon><ChatIcon /></ListItemIcon>
+//           <ListItemText primary="Chat" />
+//         </ListItem>
+//         <ListItem
+//           component={Link}
+//           to="/posts"
+//           className={`${classes.listItem} ${location.pathname === '/posts' ? classes.active : ''}`}
+//           button
+//         >
+//           <ListItemIcon><ForumIcon /></ListItemIcon>
+//           <ListItemText primary="Student Forum" />
+//         </ListItem>
+//         <ListItem
+//           component={Link}
+//           to="/posts/alumni"
+//           className={`${classes.listItem} ${location.pathname === '/posts/alumni' ? classes.active : ''}`}
+//           button
+//         >
+//           <ListItemIcon><ForumIcon /></ListItemIcon>
+//           <ListItemText primary="Alumni Forum" />
+//         </ListItem>
+//       </List>
+//       <Divider />
+//       <List>
+//         <ListItem
+//           className={`${classes.listItem}`}
+//           button
+//           onClick={logout}
+//         >
+//           <ListItemIcon><PeopleIcon /></ListItemIcon>
+//           <ListItemText primary="Log out" />
+//         </ListItem>
+//       </List>
+//     </div>
+//   );
+// };
+
+// export default Navbar;
